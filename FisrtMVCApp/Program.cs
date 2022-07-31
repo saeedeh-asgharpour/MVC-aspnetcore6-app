ba3117c1506750be5abf64c6de057629cdff762e
saeedeh-asgharpour <saeedeh.asqarpour@gmail.com>
2022-07-31 16:42:25 +0430

    mvc-example-complete

--- FisrtMVCApp/Program.cs
+++ FisrtMVCApp/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=HelloWorld}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
