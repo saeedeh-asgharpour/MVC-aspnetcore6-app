ee0ed6d6ee4f584139096768c585e3a04d533d89
saeedeh-asgharpour <saeedeh.asqarpour@gmail.com>
2022-07-31 16:47:57 +0430

    MVC-exm-final

--- FisrtMVCApp/Controllers/HelloWorldController.cs
+++ FisrtMVCApp/Controllers/HelloWorldController.cs
@@ -5,16 +5,20 @@ namespace FisrtMVCApp.Controllers
 {
     public class HelloWorldController : Controller
     {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
         public IActionResult Index()
         {
-            DogViewModel doggo = new DogViewModel()
-            { Name = "sif", Age = 2};
-            return View(doggo);
-                
+          return View(dogs);
         }
         public IActionResult Create()
-        { 
-            return View();
+        {
+            var dogVm = new DogViewModel(); 
+            return View(dogVm);
+        } 
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
         }
 
 
