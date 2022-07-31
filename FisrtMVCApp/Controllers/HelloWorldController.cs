008046ccd3e9d67416f6c1e6e835861d672eab42
saeedeh-asgharpour <saeedeh.asqarpour@gmail.com>
2022-07-31 12:34:25 +0430

    init

---
+++ FisrtMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using FisrtMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FisrtMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel()
+            { Name = "sif", Age = 2};
+            return View(doggo);
+                
+        }
+        public IActionResult Create()
+        { 
+            return View();
+        }
+
+
+        public string Hello()
+        {
+            return "whos there?";
+        }
+    }
+}
---
+++ FisrtMVCApp/Models/DogViewModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace FisrtMVCApp.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
